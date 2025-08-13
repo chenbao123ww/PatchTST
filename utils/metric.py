4a24d4c2edbf8285daa4f8ca67e9fa69c5eceb0b
chenbao123ww <chen123ww@outlook.com>
2025-08-13 15:26:44 +0800

    Update metric device

--- utils/metric.py
+++ utils/metric.py
@@ -1,32 +1,32 @@
 import torch
 import torch.nn.functional as F
 
-def masked_mse_loss(pred, target, mask=None):
+def masked_mse_loss(pred, target, mask=None, device="cpu"):
     assert pred.shape == target.shape, "Prediction and target must have the same shape."
 
-    loss = F.mse_loss(pred, target, reduction='none')
+    loss = F.mse_loss(pred, target, reduction='none').to(device)
 
     mask_num = mask.sum() if mask is not None else 0
 
     if mask is not None:
         mask = mask.unsqueeze(-1)       # [batch_size, n_feature, num_patch, 1]
-        loss = loss * mask.float()
+        loss = loss * mask.float().to(device)
 
     loss = loss.sum() / mask.sum() if mask_num > 0 else loss.mean()
 
     return loss
 
 
-def masked_mae_loss(pred, target, mask=None):
+def masked_mae_loss(pred, target, mask=None, device="cpu"):
     assert pred.shape == target.shape, "Prediction and target must have the same shape."
 
-    loss = torch.abs(pred - target)
+    loss = torch.abs(pred - target).to(device)
 
     mask_num = mask.sum() if mask is not None else 0
 
     if mask is not None:
         mask = mask.unsqueeze(-1)       # [batch_size, n_feature, num_patch, 1]
-        loss = loss * mask.float()
+        loss = loss * mask.float().to(device)
 
     loss = loss.sum() / mask.sum() if mask_num > 0 else loss.mean()
 
